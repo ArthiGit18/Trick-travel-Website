37ee3ee6a73e0248aa6506a78c4947c3a94c2521
ArthiGit18 <arthiar180@gmail.com>
2024-11-21 13:35:21 +0530

    implemented discover section

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from '../pages/Hero'
 import Navbar from '../pages/Navbar'
+import Discover from '../pages/Discover'
 
 const Main = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Discover />
     </div>
   )
 }
---
+++ src/pages/Discover.js
@@ -0,0 +1,123 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import StarRateRoundedIcon from '@mui/icons-material/StarRateRounded';
+
+const Discover = () => {
+    return (
+        <div className='discover_wrapper'>
+            <div className='container discover_section'>
+                <div className='discover_header'>
+                    <h2>Discover Weekly</h2>
+                    <p>An enim nullam tempor sapien gravida donec enim ipsum</p>
+                </div>
+                <div className='discover_actions'>
+                    <div className='discover_btns'>
+                        <Button varient="contained">Domestic</Button>
+                        <Button varient="outlines">Foreign</Button>
+                    </div>
+                    <div className='discover_arows'>
+                        <ArrowCircleLeftOutlinedIcon />
+                        <ArrowCircleRightOutlinedIcon />
+                    </div>
+                </div>
+                <div className='discover'>
+                    <div className='discover_place'>
+                        <img src="/assets/discover/1.png" alt="place" />
+                        <div className='discover_context'>
+                            <h4>Venice, Rome & Milan</h4>
+                            <div className='discover_context_actions'>
+                                <small>Karineside</small>
+                                <span>$699<span>$548</span></span>
+                            </div>
+                            <hr></hr>
+                            <div className='discover_context_actions'>
+                                <small>Tue, Jul 20 - Fri, Jul 23</small>
+                                <strong><StarRateRoundedIcon />4.9</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='discover_place'>
+                        <img src="/assets/discover/2.png" alt="place" />
+                        <div className='discover_context'>
+                            <h4>Venice, Rome & Milan</h4>
+                            <div className='discover_context_actions'>
+                                <small>Karineside</small>
+                                <span>$699<span>$548</span></span>
+                            </div>
+                            <hr></hr>
+                            <div className='discover_context_actions'>
+                                <small>Tue, Jul 20 - Fri, Jul 23</small>
+                                <strong><StarRateRoundedIcon />4.9</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='discover_place'>
+                        <img src="/assets/discover/3.png" alt="place" />
+                        <div className='discover_context'>
+                            <h4>Venice, Rome & Milan</h4>
+                            <div className='discover_context_actions'>
+                                <small>Karineside</small>
+                                <span>$699<span>$548</span></span>
+                            </div>
+                            <hr></hr>
+                            <div className='discover_context_actions'>
+                                <small>Tue, Jul 20 - Fri, Jul 23</small>
+                                <strong><StarRateRoundedIcon />4.9</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='discover_place'>
+                        <img src="/assets/discover/4.png" alt="place" />
+                        <div className='discover_context'>
+                            <h4>Venice, Rome & Milan</h4>
+                            <div className='discover_context_actions'>
+                                <small>Karineside</small>
+                                <span>$699<span>$548</span></span>
+                            </div>
+                            <hr></hr>
+                            <div className='discover_context_actions'>
+                                <small>Tue, Jul 20 - Fri, Jul 23</small>
+                                <strong><StarRateRoundedIcon />4.9</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='discover_place'>
+                        <img src="/assets/discover/5.png" alt="place" />
+                        <div className='discover_context'>
+                            <h4>Venice, Rome & Milan</h4>
+                            <div className='discover_context_actions'>
+                                <small>Karineside</small>
+                                <span>$699<span>$548</span></span>
+                            </div>
+                            <hr></hr>
+                            <div className='discover_context_actions'>
+                                <small>Tue, Jul 20 - Fri, Jul 23</small>
+                                <strong><StarRateRoundedIcon />4.9</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='discover_place'>
+                        <img src="/assets/discover/6.png" alt="place" />
+                        <div className='discover_context'>
+                            <h4>Venice, Rome & Milan</h4>
+                            <div className='discover_context_actions'>
+                                <small>Karineside</small>
+                                <span>$699<span>$548</span></span>
+                            </div>
+                            <hr></hr>
+                            <div className='discover_context_actions'>
+                                <small>Tue, Jul 20 - Fri, Jul 23</small>
+                                <strong><StarRateRoundedIcon />4.9</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Discover
